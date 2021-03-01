ab30a5a36eaa2bfa177b616606f92d98dfd8c6eb
nisingizwe joselyne <nesjoselyne@gmail.com>
2021-03-01 15:44:29 +0200

    ado

--- home/views.py
+++ home/views.py
@@ -53,19 +53,18 @@ def digitalapp (request):
             response += '1.uri mukigega \n'
             response += '2.momo isanzwe'
         elif text == '1*1':
-            response = 'CON shyiramo code yumuhinzi '+str(len(level))+' \n' 
-        elif text == '1*1'+response:
-            response = response 
-            
+            response = 'CON shyiramo code yumuhinzi '+str(level)+' \n' 
+
         elif num == '1*1' and int(len(level))==3 and str(level[2]) in str(level):
             response = 'CON shyiramo ingano yumusaruro mu biro cg litiro \n'
+
         elif num == '1*1' and int(len(level))==4 and str(level[3]) in str(level):
             response = 'CON shyiramo amafaranga ugiye kwishyura \n' 
-        elif num == '1*1' and int(len(level))==4 and str(level[3]) in str(level):
+        elif num == '1*1' and int(len(level))==5 and str(level[4]) in str(level):
             response = 'CON  wahisemo kwishyura'+ str(level[4]) + 'ugiye kwishyura kuri' + str(level[2]) +'shyiramo umubare wibanga wemeze kwishyura  \n'
         elif text == '1*2':
             response = 'CON nimero ya mobile : '+str(len(level))+ '\n'        
-            # insert=Harvestrecord(farmercode=str(level[1]))
+            # insert=Harvestrecord(farmercode=str(level[2]))
             # insert.save()
         elif num == '1*2' and int(len(level))==2 and str(level[3]) in str(level):
             response = 'CON umubare wamafaranga  \n'
